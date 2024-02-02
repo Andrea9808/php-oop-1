bdbf51b099f8ca16f0ed18dfe2f422206dc24fb3
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-02 16:10:21 +0100

    Feat: Bonus 1

--- index.php
+++ index.php
@@ -1,36 +1,68 @@
 <?php
 
+    //Classe
     class movie {
 
+         // Proprietà della classe
         public $title;
         public $genre;
+        public $year;
 
 
-        public function __construct($title, $genre){
+        // Costruttore della classe
+        public function __construct($title, $genre, $year){
 
             $this -> title = $title;
             $this -> genre = $genre;
+            $this -> year = $year;
 
         }
 
+        // Metodo per ottenere il genere del film
         public function getTitle(){
 
             return $this -> title;
 
         }
 
+        //Metodo per inserire genere
+        public function setGenre($genre){
+
+            $this -> genre = $genre;
+
+        }
+
+        // Metodo per ottenere il genere del film
+        public function getGenre(){
+
+            return $this -> genre;
+    
+        }
+
+
     }   
 
-    $movie1 = new movie("Indiana Jones","Avventura");
-    $movie2 = new movie("Harry Potter","Fantasy");
+    //Array di generi
+    $genre1 = ["Avventura","Azione"];
+    $genre2 = ["Avventura","Fantasy"];
 
-    echo("Film: " . ($movie1 -> getTitle()));
-    echo "<br>";
-    echo("Genere: " . ($movie1 -> genre));
 
-    echo "<br>.....................<br>";
+    // Array di istanze
+    $movies = array(
 
-    echo("Film: " . ($movie2 -> getTitle()));
-    echo "<br>";
-    echo("Genere: " . ($movie2 -> genre));
+        new Movie("Indiana Jones", $genre1, "2000"),
+        new Movie("Harry Potter", $genre2, "2001"),
+        new Movie("Hunger Games", $genre1, "2011"),
+
+    );
+
+    //Ciclo per stampare i film
+    foreach ($movies as $movie) {
+
+        echo "Film: " . $movie->getTitle() . "<br>";
+        echo "Genere: " .implode(', ', $movie->getGenre()) . "<br>";
+        echo "Anno: " . $movie->year . "<br>";
+        echo ".....................<br>";
+
+    }
 ?>
